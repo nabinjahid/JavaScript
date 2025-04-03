f328f244cf0ffb01a5ef3819d14b883812c26693
Md Jahidul Islam <nabinjahid@gmail.com>
2025-04-04 01:07:24 +0600

    Loop chapter done

---
+++ Loop/Loop.js
@@ -0,0 +1,344 @@
+// const numbers = [12, 98, 45, 63, 21, 72];
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+// console.log(numbers[numbers.length -1]);
+
+// const numbers = [12, 98, 45, 63, 21, 72];
+
+// for(const num of numbers){
+//     console.log(num);
+
+// }
+
+// const fruits = ['Orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+// for(const itme of fruits){
+//     console.log(itme);
+
+// }
+
+// practice
+// const subjects = ['bangla', 'english', 'Math', 'science', 'economics', 'biology', 'heigher math', 'physics']
+// for(const subject of subjects){
+//     console.log(subject);
+
+// }
+
+// const myFavFood = ['gosto', 'khasi', 'haser gosto', 'fruits', 'vagtable', 'rice']
+// for(const food of myFavFood){
+//     console.log(food);
+
+// }
+
+// const bridthday = [1978, 1983, 1996, 1998, 2002]
+// for(const year of bridthday){
+//     console.log(year);
+
+// }
+
+// const akadosh = ['Shakib', 'Miraz', 'Liton', 'Tanzid', 'Towhid', 'Mustafiz', 'Nahid', 'Taskin', 'Somya', 'Mahmudullah', 'Jahid']
+// for(const player of akadosh){
+//     console.log(player);
+
+// }
+
+// const exam = [12, 16, 19, 23, 24, 27, 31];
+// for(const date of exam){
+//     console.log(date);
+
+// }
+
+// To create a while loop five things must need
+/**
+ * 1. Declare a loop variable. This variable must have an initial value, and its value will change during the loop execution. This is called the loop variable.
+ *
+ * 2. Write the word 'while' followed by parentheses ().
+ *
+ * 3. Inside the parentheses, provide a condition. The loop variable's value will be checked against this condition. If the condition is true, the loop continues; if false, the loop stops.
+ *
+ * 4. Write some code inside the loop. This code will execute as long as the loop runs.
+ *
+ * 5. The loop variable's value must change within the loop; otherwise, the loop may run indefinitely.
+ */
+
+// let num = 0
+// while(num< 5){
+//     console.log(num);
+//     num++
+
+// }
+
+// Print you school name five times
+// let num = 0
+// while(num < 5){
+//     console.log(num,". KS School and College");
+//     num++
+// }
+
+// let num = 10;
+// while (num > 0) {
+//   console.log(num,". practice 3 hours");
+//   num--;
+// }
+
+// let numbers = 1;
+// while (numbers <= 20) {
+//   console.log(numbers);
+//   numbers++;
+// }
+
+// let numbers = 50;
+// while (numbers <= 100) {
+//   console.log(numbers);
+//   numbers++;
+// }
+
+// let numbers = 101;
+// while (numbers <= 110) {
+//   console.log(numbers/2);
+//   numbers++;
+// }
+
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++
+// }
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//   // console.log(num);
+//   sum += num
+//   num++
+// }
+// console.log(sum);
+
+// practice
+//  let num = 50
+//  while(num<=100){
+//   console.log(num);
+//   num++
+
+//  }
+
+// let num = 5
+// let sum = 0
+// while(num <=15){
+//     sum = sum + num
+//     num++
+// }
+// console.log(sum);
+
+// let num = 1
+// let sum = 50
+// while(num <= 50){
+//     sum = sum + num
+//     num++
+// }
+// console.log(sum);
+
+// let num = 21;
+// let sum = 0;
+// while (num <= 50) {
+//   console.log(num, ". sum = " , sum, '+', num ,'=>', num + sum);
+//   sum += num;
+//   console.log("total = ", sum);
+//   num++;
+// }
+
+// let num = 20;
+// let sum = 0;
+// while(num<= 40){
+//     sum+= num
+//     num++
+// }
+// console.log(sum);
+
+// For loop---------------
+
+// for (let num = 0; num < 5; num++) {
+//   console.log(num);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for(let i=50; i<= 100; i++){
+//     console.log(i);
+
+// }
+
+// let sum = 0;
+// for (let i = 11; i <= 20; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// practice
+// for(let i= 50; i<=70; i++){
+//     console.log(i);
+
+// }
+
+// let sum = 0;
+// for (let i= 31; i<= 58; i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for(let i=25; i<= 75; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// odd numbers
+// for(let i= 0; i<20 ; i++){
+//     if(i%2=== 0){
+//         console.log(i);
+
+//     }
+// }
+
+// for(let i= 0; i<=20; i++){
+//     if(i%2 === 1){
+//         console.log(i);
+
+//     }
+// }
+
+// for(let i=1; i<20; i+=2){
+//     console.log(i);
+
+// }
+// for(let i=2; i<20; i+=2){
+//     console.log(i);
+
+// }
+
+// for(let i=1; i<=30;i++){
+//     if(i%5===0){
+//         console.log(i);
+
+//     }
+// }
+
+// for(let i=1; i<= 30; i++){
+//     if(i%3===0){
+//         console.log(i);
+
+//     }
+// }
+
+// for(let i=0; i<=30; i++){
+//     if(i%3===0 || i%5===0){
+//         console.log(i);
+
+//     }
+// }
+
+// for(let i=1 ; i<=30 ; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log(i);
+
+//     }
+// }
+
+// Practice
+// for(let i=20; i<=50; i++){
+//     if(i%7===0){
+//         console.log(i);
+
+//     }
+// }
+
+// for (let i = 40; i <= 80; i++) {
+//   if (i % 5 === 0 && i % 7 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// for(let i=1; i<=40;i++){
+//     if(i%13===0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+// for(let i=1; i<=50 ; i+= 4){
+//     console.log(i);
+    
+// }
+
+// for(let i= 0; i<=100 ; i++){
+//     if(i%6===0 && i%9===0){
+//         console.log(i);
+        
+//     }
+// }
+
+// let sum = 0
+// for(let i=1; i<=50;i++){
+//     if(i%3=== 0 && i%4===0){
+//         console.log(i);
+//         sum+= i;
+        
+//     }
+// }
+// console.log(sum);
+
+
+// for(let i=0; i<=15; i++){
+//     console.log(i);
+//     if(i>7){
+        
+//         break;
+        
+//     }
+// }
+
+// for(let i=0; i<=15; i++){
+//     console.log(i);
+//     if(i>=7){
+        
+//         break;
+        
+//     }
+// }
+
+
+
+// CONTINUE------------
+// for(let i=1; i<=10;i++){
+//     if(i===6){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+// for(let i=1; i<10; i++){
+//     if(i%2===0){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+
+// for(i=1;i<10;i++){
+//     if(i%2===1){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+
+// practice
